4d4fc2ffed1076037680deb6f9a2e0359e1eeaa5
Gareth Williams <gareth.williams987@gmail.com>
2015-04-23 09:38:58 +0100

    reformat source files.

--- chapter_7/send_dear_johns_w_array_of_func_ptr.c
+++ chapter_7/send_dear_johns_w_array_of_func_ptr.c
@@ -11,36 +11,36 @@
 enum response_type { DUMP, SECOND_CHANCE, MARRIAGE, LAW_SUIT };
 
 typedef struct {
-    char *name;
-    enum response_type type;
+  char *name;
+  enum response_type type;
 } response;
 
 
 void dump(response r) {
-    printf("\nDear %s,\n", r.name);
-    puts("Unfortunately your last date contacted us to");
-    puts("say that they will not be seeing you again.");
+  printf("\nDear %s,\n", r.name);
+  puts("Unfortunately your last date contacted us to");
+  puts("say that they will not be seeing you again.");
 }
 
 
 void second_chance(response r) {
-    printf("\nDear %s,\n", r.name);
-    puts("Good news: your last date has asked us to");
-    puts("arrange another meeting. Please call ASAP.");
+  printf("\nDear %s,\n", r.name);
+  puts("Good news: your last date has asked us to");
+  puts("arrange another meeting. Please call ASAP.");
 }
 
 
 void marriage(response r) {
-    printf("\nDear %s,\n", r.name);
-    puts("Congratulations! Your last date has");
-    puts("contacted us with a proposal of marriage.");
+  printf("\nDear %s,\n", r.name);
+  puts("Congratulations! Your last date has");
+  puts("contacted us with a proposal of marriage.");
 }
 
 
 void law_suit(response r) {
-    printf("\nDear %s,\n", r.name);
-    puts("Unfortunately your last date contacted us to say");
-    puts("that they will be filing a law suit against you.");
+  printf("\nDear %s,\n", r.name);
+  puts("Unfortunately your last date contacted us to say");
+  puts("that they will be filing a law suit against you.");
 }
 
 
@@ -49,18 +49,18 @@ void law_suit(response r) {
 void (*replies[])(response) = { dump, second_chance, marriage, law_suit };
 
 int main() {
-    puts("");
-    
-    response r[] = {
-        {"Mike", DUMP}, {"Luis", SECOND_CHANCE}, {"Matt", LAW_SUIT}, {"William", MARRIAGE}
-    };
-    
-    int i;
-    for (i = 0; i < 4; i++) { // This is a function | pass it the response data r[i] 
-        (replies[r[i].type]) (r[i]);
-    }
-    
-    puts("");
-    return 0;
+  puts("");
+  
+  response r[] = {
+    {"Mike", DUMP}, {"Luis", SECOND_CHANCE}, {"Matt", LAW_SUIT}, {"William", MARRIAGE}
+  };
+  
+  int i;
+  for (i = 0; i < 4; i++) { // This is a function | pass it the response data r[i] 
+    (replies[r[i].type]) (r[i]);
+  }
+  
+  puts("");
+  return 0;
 }
     
