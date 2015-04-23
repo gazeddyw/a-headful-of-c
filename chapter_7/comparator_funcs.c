7ddca99ba9879902f9341c681fe3a436ef99e59b
Gareth Williams <gareth.williams987@gmail.com>
2015-04-23 09:39:50 +0100

    reformat source files.

--- chapter_7/comparator_funcs.c
+++ chapter_7/comparator_funcs.c
@@ -3,52 +3,46 @@
 #include "comparator_funcs.h"
 
 typedef struct {
-    int width;
-    int height;
+  int width;
+  int height;
 } rectangle;
 
 
-int compare_scores(const void* score_a, const void* score_b)
-{
-    int a = *(int*)score_a;
-    int b = *(int*)score_b;
-    return a - b;
+int compare_scores(const void* score_a, const void* score_b) {
+  int a = *(int*)score_a;
+  int b = *(int*)score_b;
+  return a - b;
 }
 
 
-int compare_scores_desc(const void* score_a, const void* score_b)
-{
-    int a = *(int*)score_a;
-    int b = *(int*)score_b;
-    return b - a;
+int compare_scores_desc(const void* score_a, const void* score_b) {
+  int a = *(int*)score_a;
+  int b = *(int*)score_b;
+  return b - a;
 }
 
 
-int compare_areas(const void* a, const void* b)
-{
-    rectangle* ra = (rectangle*)a;
-    rectangle* rb = (rectangle*)b;
-    int area_a = (ra->width * ra->height);
-    int area_b = (rb->width * rb->height);
-    return area_a - area_b;
+int compare_areas(const void* a, const void* b) {
+  rectangle* ra = (rectangle*)a;
+  rectangle* rb = (rectangle*)b;
+  int area_a = (ra->width * ra->height);
+  int area_b = (rb->width * rb->height);
+  return area_a - area_b;
 }
 
 
-int compare_areas_desc(const void* a, const void* b)
-{
-    return compare_areas(b, a);
+int compare_areas_desc(const void* a, const void* b) {
+  return compare_areas(b, a);
 }
 
 
-int compare_names(const void* a, const void* b)
-{
-    char** sa = (char**)a;
-    char** sb = (char**)b;
-    return strcmp(*sa, *sb);
+int compare_names(const void* a, const void* b) {
+  char** sa = (char**)a;
+  char** sb = (char**)b;
+  return strcmp(*sa, *sb);
 }
 
 
-int compare_names_desc(const void* a, const void* b)
-{
-    return compare_names(b, a);
+int compare_names_desc(const void* a, const void* b) {
+  return compare_names(b, a);
 }
